a7c87af2f5a52bfeddce630341b1baf40c9424fb
chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>
2024-08-27 23:53:45 +0200

    init
    Signed-off-by: chandr-andr (Kiselev Aleksandr) <chandr@chandr.net>

---
+++ c3pg0/app_config.py
@@ -0,0 +1,47 @@
+import os
+from dataclasses import dataclass
+
+import tomllib
+
+
+@dataclass
+class ApplicationConfig:
+    """Main config for the C3PG0 application."""
+
+    # Folder for migrations
+    migration_path: str | None = "./migrations"
+
+    # PostgreSQL URL
+    postgres_url: str | None = "postgres://postgres:postgres@localhost:5432/postgres"
+    postgres_url_env: str | None = "C3PG0_PSQL_URL"
+
+    # PostgresSQL URL separate envs
+    postgres_user: str | None = "postgres"
+    postgres_user_env: str | None = "C3PG0_PSQL_USER"
+
+    postgres_password: str | None = "postgres"
+    postgres_password_env: str | None = "C3PG0_PSQL_PASSWORD"
+    
+    postgres_host: str | None = "localhost"
+    postgres_host_env: str | None = "C3PG0_PSQL_HOST"
+    
+    postgres_port: int | None = 5432
+    postgres_port_env: str | None = "C3PG0_PSQL_PORT"
+
+    postgres_database_name: str | None = "postgres"
+    postgres_database_name_env: str | None = "C3PG0_PSQL_DB_NAME"
+
+    @classmethod
+    def construct(cls: type["ApplicationConfig"]) -> "ApplicationConfig":
+        """Create new ApplicationConfig from pyproject.toml configuration.
+        
+        ### Returns:
+        New `ApplicationConfig`.
+        """
+        path = f"{os.getcwd()}/pyproject.toml"
+        with open(path, mode="rb") as pyproject:
+            pyconfig = tomllib.load(pyproject)
+
+        c3pg0_config = pyconfig["tool"].get("c3pg0")
+
+        return ApplicationConfig(**c3pg0_config if c3pg0_config else {})
---
+++ c3pg0/cli.py
@@ -0,0 +1,58 @@
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
+from dataclasses import dataclass
+from typing import Optional, Sequence, Type, Union
+from typing_extensions import Annotated
+
+import typer
+
+from c3pg0.app_config import ApplicationConfig
+
+
+app = typer.Typer()
+
+
+@app.command()
+def apply(
+    version: Annotated[
+        str,
+        typer.Option(help="New version for the migration."),
+    ],
+) -> None:
+    print(ApplicationConfig.construct())
+    """Apply new migration."""
+
+
+@app.command()
+def rollback(
+    version: Annotated[
+        str,
+        typer.Option(help="Version for rollback."),
+    ],
+) -> None:
+    """Rollback database to specified version."""
+
+
+@app.command()
+def create(
+    name: Annotated[
+        str,
+        typer.Argument(help="Name for the revision."),
+    ],
+    apply_in_transaction: Annotated[
+        Optional[bool],
+        typer.Option(help="Execute in transaction or not."),
+    ] = True,
+    rollback_in_transaction: Annotated[
+        Optional[bool],
+        typer.Option(help="Execute in transaction or not."),
+    ] = True,
+    is_python: Annotated[
+        Optional[bool],
+        typer.Option(help="Is it .py file of not?"),
+    ] = False,
+) -> None:
+    """Create new migration."""
+        
+
+if __name__ == "__main__":
+    app()
